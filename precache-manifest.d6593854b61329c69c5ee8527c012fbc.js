9929087b383f82cbb822322b4c96c8b3c252cd74
Jun Du <junqdu@gmail.com>
2019-12-26 23:48:25 -0800

    Updates

---
+++ precache-manifest.d6593854b61329c69c5ee8527c012fbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5334c42393938005246ab411e458d27e",
+    "url": "/za-reshipi/index.html"
+  },
+  {
+    "revision": "eaec5bed9ae3b2ae7e0a",
+    "url": "/za-reshipi/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f6321bb8d920477ffa1a",
+    "url": "/za-reshipi/static/js/2.92630e58.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/za-reshipi/static/js/2.92630e58.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eaec5bed9ae3b2ae7e0a",
+    "url": "/za-reshipi/static/js/main.28926684.chunk.js"
+  },
+  {
+    "revision": "60c409c5253bf94d2668",
+    "url": "/za-reshipi/static/js/runtime-main.2e4121fb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/za-reshipi/static/media/logo.5d5d9eef.svg"
+  }
+]);
